9fb862457ec0ae7157a0a178dcc8ddfd8d8c6ebe
Kaleb Lindevaldsen <kflindevaldsen@liberty.edu>
2020-08-23 02:17:34 -0400

    added home screen and button

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Kaleb Lindevaldsen on 8/23/20.
+//  Copyright © 2020 Kaleb Lindevaldsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
