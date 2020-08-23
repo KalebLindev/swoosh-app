d5e8e406dd5302b274e88c110a9beb28bef1d0d6
Kaleb Lindevaldsen <kflindevaldsen@liberty.edu>
2020-08-23 12:26:25 -0400

    lesson 6 refactoring and re-naming

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Kaleb Lindevaldsen on 8/23/20.
+//  Copyright © 2020 Kaleb Lindevaldsen. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
